2e7d69a5ddd33d469766ba0c1a6a179c49af4729
Jared Norman <jared@creepywizard.com>
2017-08-05 11:41:37 -0700

    Looks like some stuff

---
+++ init.el
@@ -0,0 +1,37 @@
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(require 'evil)
+(evil-mode 1)
+
+(use-package magit
+  :ensure t)
+
+(use-package helm
+  :ensure t)
+
+(use-package base16-theme
+  :ensure t)
+
+(load-theme 'base16-unikitty-light t)
+
+(tool-bar-mode -1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package evil-visual-mark-mode ##))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
