5b73e46e27a811f93c85f9e9fec0af088366011e
Jared Norman <jared@creepywizard.com>
2017-08-05 12:57:42 -0700

    Added in a bunch more things nice

---
+++ jardo/jardo-diminish.el
@@ -0,0 +1,6 @@
+(use-package diminish
+  :ensure t
+  :config
+  (diminish 'which-key-mode))
+
+(provide 'jardo-diminish)
--- jardo/jardo-evil.el
+++ jardo/jardo-evil.el
@@ -3,4 +3,9 @@
   :config
   (evil-mode 1))
 
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+
 (provide 'jardo-evil)
