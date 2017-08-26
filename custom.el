5c9c5a46a14ccdc806b2e091ddc444f152c7537f
Jared Norman <jared@creepywizard.com>
2017-08-26 14:46:22 -0700

    Whatever

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(package-selected-packages
    (quote
-    (web-mode tide typescript-mode cider company company-mode chruby enh-ruby-mode flycheck helm-projectile projectile spaceline spaceline-config use-package evil-visual-mark-mode ##))))
+    (coffeescript-mode js2-mode web-mode tide typescript-mode cider company company-mode chruby enh-ruby-mode flycheck helm-projectile projectile spaceline spaceline-config use-package evil-visual-mark-mode ##))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
