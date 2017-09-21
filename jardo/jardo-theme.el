fee2fd227a270e5504d5e36f79fb0f47596b0f18
Jared Norman <jared@creepywizard.com>
2017-09-21 09:05:29 -0700

    Switch to Fira Mono

--- jardo/jardo-theme.el
+++ jardo/jardo-theme.el
@@ -3,6 +3,6 @@
   :config
   (load-theme 'gruvbox-dark-medium t)
   (set-face-attribute 'default nil
-		      :family "Menlo" :height 150 :weight 'normal))
+		      :family "Fira Mono" :height 150 :weight 'normal))
 
 (provide 'jardo-theme)
