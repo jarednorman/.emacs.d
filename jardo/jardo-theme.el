ed6a648f8415c11b0fc455e65e3b0c96f5c18c21
Jared Norman <jared@creepywizard.com>
2018-02-16 09:32:31 -0800

    Shrink default font... too many codes to see

--- jardo/jardo-theme.el
+++ jardo/jardo-theme.el
@@ -3,6 +3,6 @@
   :config
   (load-theme 'leuven t)
   (set-face-attribute 'default nil
-                      :family "Office Code Pro" :height 140 :weight 'normal))
+                      :family "Menlo" :height 130 :weight 'normal))
 
 (provide 'jardo-theme)
